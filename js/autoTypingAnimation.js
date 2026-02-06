ff6a6da69f773dd2fb0848cce9803e9f922b1e98
Dronzer25 <99458657+Dronzer25@users.noreply.github.com>
2026-02-06 07:19:24 +0000

    Made changes

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Jagadish Mohanty</span> Completed Graduation in IT with skills Enterprise Java , MYSQL , C# , MongoDB , C++ and Honing it more. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and    problem solving attitude. Eagerly looking forward to learn and grow."
+        "<span>Hi, I am Jagadish Mohanty</span> Completed Post-Graduation in IT with skills Enterprise Java , MYSQL , C# , MongoDB , C++ and Honing it more. An adaptable and responsible Student seeking an entry into IT Sector. An excellent academic record with active participation in activities shows myself to be selfmotivated, organized and capable of working under pressure. Science studies gave me a logical mind and    problem solving attitude. Eagerly looking forward to learn and grow."
       ],
       typeSpeed: 7,
       contentType: 'html'
